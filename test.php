dcf994a2b60b6a704a2cd67af513ebbdeccc3d10
Patrick Lumumba <97313939+lumumbapl@users.noreply.github.com>
2024-02-27 22:45:07 +0300

    Update test.php

--- test.php
+++ test.php
@@ -5,10 +5,10 @@ Version: 1.0.0
 */
 wp_register_script_module(
 'bar',
-plugins_url( '/bar.js', FILE )
+plugins_url( '/bar.js', _FILE_ )
 );
 wp_enqueue_script_module(
 'foo',
-plugins_url( '/foo.js', FILE ),
+plugins_url( '/foo.js', _FILE_ ),
 array( 'bar' )
 );
